6012390dbd57480bce80eaed3cbba69d22d6037c
Elena Komova <e.komova01@gmail.com>
2024-08-05 20:18:57 +0300

    change v.2 Week 1 Day 2

--- Week 1/Day 2/ExerciseXP/Exercise4.py
+++ Week 1/Day 2/ExerciseXP/Exercise4.py
@@ -1,14 +1,15 @@
 # Recap – What is a float? What is the difference between an integer and a float?
+#integer is a number without a decimal point. 
 # Create a list containing the following sequence 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5 (don’t hard-code the sequence).
 # Can you think of another way to generate a sequence of floats?
 
-step = 0,5
+step = 0.5
 last_numder = 5
-
 sequence = []
-number = 1,5
+number = 1.5
 
 while number <= last_numder:
     sequence.append(number)
+    number+=step
 
 print(sequence)    
