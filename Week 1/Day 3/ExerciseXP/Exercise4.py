37e565311419eb38a92d6b70b411e597d1f747d7
Elena Komova <e.komova01@gmail.com>
2024-08-06 15:35:24 +0300

    Add v.5 Week 1 Day 3

--- Week 1/Day 3/ExerciseXP/Exercise4.py
+++ Week 1/Day 3/ExerciseXP/Exercise4.py
@@ -45,7 +45,25 @@ users_sort = sorted(users)
 for i in range(len(users_sort)):
      disney_users_C[users_sort[i]] = i
 
-print(disney_users_C)   
+print(disney_users_C)  
+
+disney_users_A_i = {}
+
+for i in range(len(users)):
+     if "i" in users[i]:
+      disney_users_A_i[users[i]] = i
+
+print(disney_users_A_i)
+
+disney_users_A_f = {}
+
+for i in range(len(users)):
+     if users[i][0] == "M" or users[i][0] == "P":
+      disney_users_A_f[users[i]] = i
+
+print(disney_users_A_f)
+
+     
 
 
 
