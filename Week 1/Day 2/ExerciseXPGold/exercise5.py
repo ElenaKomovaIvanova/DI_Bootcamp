267d35dad7af12a402d5a338e38badc9e8bf9a15
Elena Komova <e.komova01@gmail.com>
2024-08-06 10:53:12 +0300

    add v.3 Gold Week 1 Day 2

--- Week 1/Day 2/ExerciseXPGold/exercise5.py
+++ Week 1/Day 2/ExerciseXPGold/exercise5.py
@@ -1,2 +1,17 @@
 # Create a string of all the letters in the alphabet
 # Loop over each letter and print a message that contains the letter and whether its a vowel or a consonant.
+
+import string
+
+
+alphabet = string.ascii_lowercase
+vowels = "aeiou"
+list_letters = []
+
+for i in range(len(alphabet)):
+    if alphabet[i] in vowels:
+        list_letters.append(f"{alphabet[i]}: vowel")
+    else: list_letters.append(f"{alphabet[i]}: consonant")
+
+
+print(list_letters)
