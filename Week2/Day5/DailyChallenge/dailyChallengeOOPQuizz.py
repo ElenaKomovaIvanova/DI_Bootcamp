0a24d189f1a4428ff7ed7578f26f5c180224a447
Elena Komova <e.komova01@gmail.com>
2024-08-16 13:42:48 +0300

    add DG2 Week 2 Day 5

--- Week2/Day5/DailyChallenge/dailyChallengeOOPQuizz.py
+++ Week2/Day5/DailyChallenge/dailyChallengeOOPQuizz.py
@@ -1,3 +1,21 @@
+# What is a class?
+# A class is a template with which it is convenient to describe objects of the same type.
+# What is an instance?
+# an object that has access to the entire contents of the class, but also has the ability to store its own data.
+# What is encapsulation?
+# This is the principle according to which the internal structure of entities must be combined in a special 
+# “shell” and hidden from outside interference.
+# What is abstraction?
+# it's like simplifying a real object down to its key characteristics.
+# What is inheritance?
+# allows you to define a child class that uses (inherits), extends, or modifies the capabilities of the parent class.
+# What is multiple inheritance?
+# when a class can have more than one superclass
+# What is polymorphism?
+# allows different entities to perform the same actions
+# What is method resolution order or MRO?
+# allows Python to figure out from which ancestor class a method should be called if it is not found directly 
+# in the descendant class
 import random
 
 
