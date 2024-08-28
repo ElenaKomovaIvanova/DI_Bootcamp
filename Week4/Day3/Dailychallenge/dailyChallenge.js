373c3d8a6b248c53504a6296b90060c9631df004
Elena Komova <e.komova01@gmail.com>
2024-08-28 09:55:04 +0300

    added DG week4 day3

--- Week4/Day3/Dailychallenge/dailyChallenge.js
+++ Week4/Day3/Dailychallenge/dailyChallenge.js
@@ -1,45 +1,40 @@
 const planets = [
-    'Mercury',
-    'Venus',
-    'Earth',
-    'Mars',
-    'Jupiter',
-    'Saturn',
-    'Uranus',
-    'Neptune',
-    'Pluto'
+    { name: 'Mercury', color: 'gray', moons: 0 },
+    { name: 'Venus', color: 'yellow', moons: 0 },
+    { name: 'Earth', color: 'green', moons: 1 },
+    { name: 'Mars', color: 'red', moons: 2 },
+    { name: 'Jupiter', color: 'orange', moons: 79 },
+    { name: 'Saturn', color: 'goldenrod', moons: 83 },
+    { name: 'Uranus', color: 'lightblue', moons: 27 },
+    { name: 'Neptune', color: 'blue', moons: 14 },
+    { name: 'Pluto', color: 'purple', moons: 4 },
 ];
+
+const section = document.querySelector('.listPlanets');
 
 planets.forEach(planet => {
     const planetDiv = document.createElement('div');
-    planetDiv.classList.add(planet);
-    planetDiv.textContent = planet;
-    document.body.appendChild(planetDiv)
+    planetDiv.classList.add('planet');
+    planetDiv.style.backgroundColor = planet.color;
+
+    const planetText = document.createElement('p');
+    planetText.textContent = planet.name;
+    planetDiv.appendChild(planetText);
+
+    const distanceFromPlanet = 60; 
+
+    for (let i = 0; i < planet.moons; i++) {
+        const moonDiv = document.createElement('div');
+        moonDiv.classList.add('moon');
+        const angle = (i / planet.moons) * 2 * Math.PI; 
+        const moonX = distanceFromPlanet * Math.cos(angle);
+        const moonY = distanceFromPlanet * Math.sin(angle);
+
+        moonDiv.style.left = `${50 + moonX}px`;
+        moonDiv.style.top = `${50 + moonY}px`;
+
+        planetDiv.appendChild(moonDiv);
+    }
+
+    section.appendChild(planetDiv);
 });
-
-const planetMercury = document.getElementsByClassName('Mercury')
-planetMercury[0].style.backgroundColor = 'blue';
-
-const planetVenus = document.getElementsByClassName('Venus')
-planetVenus[0].style.backgroundColor = 'pink';
-
-const planetEarth = document.getElementsByClassName('Earth')
-planetEarth[0].style.backgroundColor = 'green';
-
-const planetMars = document.getElementsByClassName('Mars')
-planetMars[0].style.backgroundColor = 'red';
-
-const planetJupiter = document.getElementsByClassName('Jupiter')
-planetJupiter[0].style.backgroundColor = 'orange';
-
-const planetSaturn = document.getElementsByClassName('Saturn')
-planetSaturn[0].style.backgroundColor = 'grey';
-
-const planetUranus = document.getElementsByClassName('Uranus')
-planetUranus[0].style.backgroundColor = 'lightblue';
-
-const planetNeptune = document.getElementsByClassName('Neptune')
-planetNeptune[0].style.backgroundColor = 'indigo';
-
-const planetPluto = document.getElementsByClassName('Pluto')
-planetPluto[0].style.backgroundColor = 'purple';
